5b712bc1a2d4a6e463dd89e3da0d4b4b56abc150
Ed Harris <ed.harris@asos.com>
2021-12-21 14:31:13 +0000

    add bolean argument to control data output format

--- basicModel/modules/test_dataloader.py
+++ basicModel/modules/test_dataloader.py
@@ -41,18 +41,26 @@ def data_to_dict(
     ### create a dictionary tensor dataframe as input into the model
     df_list: list,
     df_len: int,
-    chosen_features: list
+    chosen_features: list,
+    as_dict = False
 ):
     df_list = (tf.stack([df_list[i][0] for i in range(df_len)]),tf.stack([df_list[i][-1] for i in range(df_len)]))
     dict_features = {}
-    # dict_targets = {}
+    dict_target = {}
+    dict = {}
     if len(chosen_features) == 1:
         dict_features[chosen_features[0]] = df_list[0]
-        dict_features['target'] = df_list[-1]
+        dict[chosen_features[0]] = df_list[0]
+        dict_target['target'] = df_list[-1]
+        dict['target'] = df_list[-1]
     else:
         for i in range(len(df_list[0]),-1):
             dict_features[chosen_features[i]] = df_list[0][i]
-    return dict_features
+    if as_dict == False:
+        return_value = (dict_features,dict_target)
+    elif as_dict == True:
+        return_value = dict
+    return return_value
 
 def get_test_dataset(
     ### function to call all other functions necessary to build the dataset
@@ -66,7 +74,7 @@ def get_test_dataset(
     df = pad(df)
     df = ds_from_df(df)
     # df = pad_dataset(df,df_len)
-    df = data_to_dict(list(df),df_len,chosen_features)
+    df = data_to_dict(list(df),df_len,chosen_features,as_dict)
     if as_dict == False:
         df = tf.data.Dataset.from_tensor_slices(df).batch(batch_size)
     steps = int(np.floor(df_len/batch_size))
